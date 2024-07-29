082ea806a33be2ed491f763ef956573dee2b0386
Hayk8637 <62342210+Hayk8637@users.noreply.github.com>
2024-07-29 16:17:24 +0400

    a

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,5 +1,5 @@
 import './style.css'
-import img from './Picsart_24-07-28_02-00-45-834.jpg'
+// import img from './Picsart_24-07-28_02-00-45-834.jpg'
 const Banner:React.FC = () => {
     return <div className="banner">
             <div className='text'>
